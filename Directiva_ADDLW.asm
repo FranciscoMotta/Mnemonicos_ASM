68fa18ad02da36ba8f31f14d3cb609659d05128d
Julio Francisco Motta <jmottaclaudio@gmail.com>
2020-06-06 02:28:49 -0500

    Añadimos algunas explicaciones

--- Directiva_ADDLW.asm
+++ Directiva_ADDLW.asm
@@ -17,5 +17,9 @@ END
 MAIN:; damos el alías de la rutina principal
     MOVLW .20; Movemos el dato literal 20 hacia el registro W de trabajo
     ADDLW .10, W; sumamos el valor de W con el dato literal 10 y se guarda en W
+    ; Acá podemos aplicar otra diferencia es es porque el valor de la suma 
+    ;No se puede guardar en F, debido a al poner F se supondría que debe guardar 
+    ;En la variable parte de la suma, pero en este caso, ese lugar lo toma un 
+    ;dalo literal.
     MOVWF auxiliar; Movemos el valor de W hacía la variable auxiliar 
     END 
